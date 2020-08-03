cff4a5c5d5ab343999b2a192a8fbc7549f882063
NanaKay007 <jnranikuabe@gmail.com>
2020-08-03 10:49:29 -0400

    simplified date extraction

--- js/utils.js
+++ js/utils.js
@@ -1,18 +1,12 @@
-function shortenText(text, startingPoint, maxLength) {
-    return text.length > maxLength ?
-        text.slice(startingPoint, maxLength) :
-        text
-}
-
 function postCard(post) {
     return `
     <section class="mzp-c-card mzp-has-aspect-16-9 mzp-c-card-extra-small">
        <a class="mzp-c-card-block-link" rel="noopener noreferrer" href="${post.link}">
          <div class="mzp-c-card-media-wrapper">
-           <img src="${post.thumbnail}" class="mzp-c-card-image">
+           <img src="${post.thumbnail}" alt="" class="mzp-c-card-image">
          </div>
           <div class="mzp-c-card-content">
-           <div class="mzp-c-card-tag">${shortenText(post.pubDate, 0, 10)}</div>
+           <div class="mzp-c-card-tag">${post.pubDate.split(" ")[0]}</div>
            <h2 class="mzp-c-card-title">${post.title}</h2>
           </div>
        </a>
@@ -22,7 +16,6 @@ function postCard(post) {
 function renderPosts(data,postElemClass) {
     // Filter for actual posts. Comments don't have categories, therefore can filter for items with categories bigger than 0
     const posts = data.slice(0, 4).filter(item => item.categories.length > 0) // That's the main trick* !
-
     // Put things in right spots of card
     let output = '';
     posts.forEach((item) => {
