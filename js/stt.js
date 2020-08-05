f5ca82be41d0672547684d2ef92078e1d20b3db3
NanaKay007 <jnranikuabe@gmail.com>
2020-08-05 15:00:27 -0400

    refactored so that same news will be loaded on other pages

--- js/stt.js
+++ js/stt.js
@@ -1,16 +1,3 @@
-let mz_blog = 'https://blog.mozilla.org/blog/category/emerging-tech/feed';
-let moz_hacks = 'https://hacks.mozilla.org/author/rmoraismozilla-com/feed';
-var api = 'https://api.rss2json.com/v1/api.json?rss_url=';
-
 window.addEventListener('DOMContentLoaded', () => {
-    function sortByDate(a, b) {
-        return new Date(b.pubDate) - new Date(a.pubDate)
-    }
-
-    let urls = [`${api}${mz_blog}`,`${api}${moz_hacks}`]
-                .map(url => fetch(url).then(res => res.json()));
-                
-    Promise.all(urls).then(async res => {
-        return res.map(obj => obj.items).flat().sort(sortByDate);
-    }).then( res => renderPosts(res,'js-stt-blog'))
+    loadPosts('js-stt-blog')
 });
